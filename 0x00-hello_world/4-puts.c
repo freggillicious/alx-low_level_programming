fe98599f525b9ca756d262dc69f44e33605fe3b2
freggy <fregenegoodluck@gmail.com>
2022-09-10 02:12:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  *  Description: main - function main begins the execution of the program
- *
- *   *  Return: 0 indicates that the programme is successful
- *
- *    */
-
+ *  Description: main - function main begins the execution of the program
+ *  Return: 0 indicates that the programme is successful
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
