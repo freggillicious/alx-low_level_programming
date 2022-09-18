947c309873288fb60eda156b29ccdde8de62f416
freggy <fregenegoodluck@gmail.com>
2022-09-18 09:57:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *main-main block
- *Description:print -putchar in a new line
- *Return:this program returns zero
- */
+*main-main block
+*Description:print -putchar in a new line
+*Return:this program returns zero
+*/
 int main(void)
 {
 _putchar('_');
